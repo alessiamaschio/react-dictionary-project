7da4a82ad576137aab8dc50dddea530420f59f03
Alessia <alessiamaschio@gmail.com>
2021-04-20 11:30:09 +0200

    Api integration

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
+import axios from "axios";
+import swal from "sweetalert";
+
 import "./Search.css";
 
 export default function Search(event) {
   const [input, setInput] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function searchWord(event) {
     event.preventDefault();
-    alert(`Searching for ${input}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${input}`;
+    axios
+      .get(apiUrl)
+      .then(handleResponse)
+      .catch(() =>
+        swal({
+          title: "Error, error...",
+          text: "Are you sure you typed the word correctly? 😉",
+          icon: "error",
+          button: "Try again!",
+        })
+      );
   }
 
   function handleInputChange(event) {
     setInput(event.target.value);
   }
+
   return (
     <form onSubmit={searchWord}>
       <input
