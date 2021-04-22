59f528ddcaf9d4ef2b8b4ed975b23374476674c1
Alessia <alessiamaschio@gmail.com>
2021-04-22 13:59:23 +0200

    Add phonetic play sound

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,19 +1,26 @@
 import React from "react";
+import useSound from "use-sound";
 
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
+  let soundUrl = props.value.audio;
+
+  const [play] = useSound(soundUrl, {
+    volume: 0.5,
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    play();
+  }
   console.log(props.value);
+
   return (
     <div className="Phonetics">
       <div>
         <span>
-          <a
-            className="Phonetic-link"
-            href={props.value.audio}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="Phonetic-link" href="/" onClick={handleClick}>
             {props.value.text}
           </a>
         </span>
